d9ebe54361c4df9b8d44bf77903061b453fa1a7d
Amaan-sde <amaankhan172595@gmail.com>
2026-09-02 23:43:03 +0530

    Initialize Express in index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const app = require('./src/app');
 const config = require('./src/config');
 const logger = require('./src/utils/logger');
+const express = require('express');
+
+const app = express();
 
 const server = app.listen(config.port, () => {
   logger.info(`Server is running at http://localhost:${config.port} in ${config.env} mode`);
